5e24bb0e92cd1bf335801525cc1091352d2cd31c
limhao2705 <limhao2705@gmail.com>
2024-04-29 14:26:50 +0700

    add the content for community header

---
+++ resources/views/unauth/header-unauth/community.blade.php
@@ -0,0 +1,16 @@
+@extends("unauth.layouts.layout")
+
+@section('header')
+    <nav>
+        <ul>
+            <li><a href="{{route('catalog')}}">Catalog</a></li>
+            <li><a href="{{route('doc')}}">Documentation</a></li>
+            <li><a class="active" href="{{route('community')}}">Community</a></li>
+            <li><a href="{{route('pricing')}}">Pricing</a></li>
+        </ul>
+    </nav>
+@endsection
+
+
+@section('content')
+@endsection
