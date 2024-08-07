0fc7843f78e5c0f210b44744a31e4f3e40ecb799
Hout-Manut <huotmanut00@gmail.com>
2024-08-07 20:57:37 +0700

    Made an HTML highlighter (No comments yet)

--- public/assets/js/codeblock.js
+++ public/assets/js/codeblock.js
@@ -1,6 +1,38 @@
-console.log("hi")
-let codeBlocks = document.querySelectorAll("pre code");
+class HTML {
+    static quotePattern = new RegExp(/"/, "g");
+    static tagPattern = new RegExp(/&lt;(\/)?(\w+)/, "gi");
+    static attributePattern = new RegExp(/(\s+\w+)=&quot;/, "gi");
+    static valuePattern = new RegExp(/&quot;(.*?)&quot;/, "gi");
+    static commentPattern = new RegExp(/&lt;!--(.*?)(--&gt;)/, "gis");
 
-codeBlocks.forEach((block) => {
-    console.log(block);
+    static highlight(block) {
+        console.log(block);
+        block = block.replace(this.quotePattern, '&quot;');
+        console.log(block);
+
+        block = block.replace(this.tagPattern, (match, slash, value) => {
+            slash = slash ? slash : "";
+            return `&lt;${slash}<span class="tag">${value}</span>`;
+        });
+
+        block = block.replace(this.attributePattern, (match, value) => {
+            return `<span class="attribute">${value}</span>=&quot;`;
+        });
+
+        block = block.replace(this.valuePattern, (match, value) => {
+            return `&quot;<span class="value">${value}</span>&quot;`;
+        })
+        return block;
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const codeBlocks = document.querySelectorAll("pre code");
+
+    codeBlocks.forEach(block => {
+        if (block.classList.contains("language-html")) {
+            block.innerHTML = HTML.highlight(block.innerHTML);
+        }
+    });
 });
